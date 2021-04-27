614e0cd36821a4efee72b6f9338a6dec48c03e4e
Jacob Merioles <>
2021-04-27 16:41:45 +0000

    finished Unit 8 lab 2b

--- ProfNames.java
+++ ProfNames.java
@@ -1,9 +1,13 @@
+//Programmer: Jacob Merioles Period 2 APCSA
 public class ProfNames
 {
   public int countNames(String[][]nameArray, String lastName)
   {
-
-
-    return 0;
+    int count = 0;
+    for (String[] row: nameArray)
+      for (String name: row)
+        if (name.contains("Smith"))
+          count++;
+    return count;
   }
 }
